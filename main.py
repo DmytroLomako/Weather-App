a53a5623139b240b5a0e9ee3ded838f3afb01dcb
DmytroLomako <lomakodmytro@gmail.com>
2024-09-19 21:34:45 +0300

    Weather pictures

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ import PIL
 import os
 import json
 from deep_translator import GoogleTranslator
+from datetime import datetime
 
 translator = GoogleTranslator(source = 'auto', target = 'uk')
-api = '752ea81eb24913b6bd544daeca993f8f'
+api = 
 # city = input("Enter a city: ")
 # url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api}'
 # answer = requests.get(url)
@@ -96,20 +97,28 @@ def exit_account(label_user_text, frame):
 
 list_weather_time = []
 list_weather_temp = []
+list_weather_icon = []
 url = f'https://api.openweathermap.org/data/2.5/forecast?q={user_city}&appid={api}'
 answer = requests.get(url)
 if answer.status_code == 200:
     data = answer.json()
     temp = data['list'][0]['main']['temp'] - 273.15
     temp = round(temp, 1)
+    icon = data['list'][0]['weather'][0]['icon']
     feels_like = round(data['list'][0]['main']['feels_like'] - 273.15, 1)
     wind_speed = data['list'][0]['wind']['speed']
     humidity = data['list'][0]['main']['humidity']
+    weather_description_main = data['list'][0]['weather'][0]['main'].title()
     weather_desription = data['list'][0]['weather'][0]['description'].title()
+    time = data['list'][0]['dt_txt'].split(' ')[0]
+    day = time.split('-')[1]
+    month = time.split('-')[2]
+    year = time.split('-')[0].split('20')[1]
     dt_txt = 'dt_txt'
     for dt_txt in data['list']:
         list_weather_time.append(dt_txt['dt_txt'].split(' ')[1].split(':')[0])
         list_weather_temp.append(round(dt_txt['main']['temp'] - 273.15, 1))
+        list_weather_icon.append(dt_txt['weather'][0]['icon'])
     # date_time = data['list'][]
 app = CTk()
 app.title('Weather App')
@@ -140,43 +149,161 @@ search_loope = CTkLabel(frame_weather, image = image3, text = '', bg_color = '#0
 search_loope.place(x = 831, y = 51)
 frame_current_position = CTkFrame(frame_weather, 315, 275, fg_color = '#5DA7B1')
 frame_current_position.place(x = 300, y = 120)
+weekdays = ['Понеділок', 'Вівторок', 'Середа', 'Четвер', "П'ятниця", 'Субота', 'Неділя']
 def load_weather():
-    global current_city_text, current_temp_text, weather_desription, current_description_text, list_first_hours_temp, list_first_hours_time
+    global current_city_text, current_temp_text, weather_desription, current_description_text, list_first_hours_temp, list_first_hours_time, current_time_func, current_city_select_text, range_count, right_arrow, current_description_select_text, current_temp_select_text, max_temp, min_temp, current_city_select_max_temp_text, weather_description_main
     current_position_text = CTkLabel(frame_current_position, text = 'Поточна позиція', font = ('Arial', 35), text_color = 'white')
     current_position_text.grid(row = 0, column = 0, pady = 10)
     current_city_text = CTkLabel(frame_current_position, text = user_city, font = ('Arial', 22), text_color = 'white')
     current_city_text.grid(row = 1, column = 0, pady = 10)
     current_temp_text = CTkLabel(frame_current_position, text = f'{temp}°', font = ('Arial', 48), text_color = 'white')
     current_temp_text.grid(row = 2, column = 0, pady = 10)
+    path_to_image = os.path.abspath(__file__ + f'/../weather/{icon}.png')
+    if not os.path.exists(path_to_image):
+        path_to_image = os.path.abspath(__file__ + f'/../weather/01d.png')
+        print('Не знайдено картинку', list_weather_icon[0])
+    image = PIL.Image.open(path_to_image)
+    image = image.resize([180, 180])
+    image = PIL.ImageTk.PhotoImage(image)
+    current_image = CTkLabel(frame_weather, image = image, text = '')
+    current_image.place(x = 75, y = 180)
     weather_desription = translator.translate(weather_desription)
-    current_description_text = CTkLabel(frame_current_position, text = weather_desription, font = ('Arial', 22), text_color = 'white')
+    weather_description_main = translator.translate(weather_description_main).title()
+    current_description_text = CTkLabel(frame_current_position, text = weather_description_main, font = ('Arial', 22), text_color = 'white')
     current_description_text.grid(row = 3, column = 0, pady = 10)
-    frame_hourly_weather = CTkFrame(frame_weather, 820, 240, fg_color = '#096C82')
-    frame_hourly_weather.place(x = 50, y = 430)
-    print(3)
+    current_time = CTkLabel(frame_weather, text = f'{day}.{month}.{year}', font = ('Arial', 40), text_color = 'white')
+    current_time.place(x = 665, y = 240)
+    frame_hourly_weather_main = CTkFrame(frame_weather, 785, 250, fg_color = '#5DA7B1', border_color = '#b5d3d9', border_width = 5, corner_radius = 20)
+    frame_hourly_weather_main.place(x = 70, y = 430)
+    frame_hourly_weather = CTkFrame(frame_hourly_weather_main, 20, 240, fg_color = 'transparent')
+    frame_hourly_weather.place(x = 20, y = 70)
+    top_line = CTkFrame(frame_hourly_weather_main, 764, 2, fg_color = '#b5d3d9')
+    top_line.place(x = 18, y = 50)
+    current_weather_description = CTkLabel(frame_hourly_weather_main, text = f'{weather_desription}', font = ('Arial', 20), text_color = 'white')
+    current_weather_description.place(x = 30, y = 20)
+    range_count = [0, 1, 2, 3, 4, 5, 6, 7]
+    def right_arrow_func(event):
+        global range_count, list_first_hours_temp, list_first_hours_time, right_arrow
+        if range_count[7] == 39:
+            return False
+        left_arrow.configure(text_color = 'white')
+        range_count.append(range_count[7] + 1)
+        del range_count[0]
+        j = 0
+        for i in range_count:
+            list_first_hours_time[j].configure(text = f'{list_weather_time[i]}:00')
+            list_first_hours_temp[j].configure(text = f'{list_weather_temp[i]}°')
+            path_to_image = os.path.abspath(__file__ + f'/../weather/{list_weather_icon[i]}.png')
+            if not os.path.exists(path_to_image):
+                path_to_image = os.path.abspath(__file__ + f'/../weather/01d.png')
+                print('Не знайдено картинку', list_weather_icon[0])
+            image = PIL.Image.open(path_to_image)
+            image = image.resize([50, 50])
+            image = PIL.ImageTk.PhotoImage(image)
+            list_weather_icon2[j].configure(image = image)
+            j += 1
+        if range_count[7] == 39:
+            right_arrow.configure(text_color = '#a0bcbd')
+    def left_arrow_func(event):
+        global range_count, list_first_hours_temp, list_first_hours_time, right_arrow
+        if range_count[0] == 0:
+            return False
+        right_arrow.configure(text_color = 'white')
+        range_count.insert(0, range_count[0] - 1)
+        del range_count[8]
+        j = 0
+        for i in range_count:
+            list_first_hours_time[j].configure(text = f'{list_weather_time[i]}:00')
+            list_first_hours_temp[j].configure(text = f'{list_weather_temp[i]}°')
+            path_to_image = os.path.abspath(__file__ + f'/../weather/{list_weather_icon[i]}.png')
+            if not os.path.exists(path_to_image):
+                path_to_image = os.path.abspath(__file__ + f'/../weather/01d.png')
+                print('Не знайдено картинку', list_weather_icon[0])
+            image = PIL.Image.open(path_to_image)
+            image = image.resize([50, 50])
+            image = PIL.ImageTk.PhotoImage(image)
+            list_weather_icon2[j].configure(image = image)
+            j += 1
+        if range_count[0] == 0:
+            left_arrow.configure(text_color = '#a0bcbd')
+    right_arrow = CTkLabel(frame_weather, text = '>', font = ('Arial', 50), text_color = 'white')
+    right_arrow.place(x = 875, y = 520)
+    right_arrow.bind('<Button-1>', right_arrow_func)
+    left_arrow = CTkLabel(frame_weather, text = '<', font = ('Arial', 50), text_color = '#a0bcbd')
+    left_arrow.place(x = 20, y = 520)
+    left_arrow.bind('<Button-1>', left_arrow_func)
+    weekday_text = CTkLabel(frame_weather, text = weekdays[datetime.now().weekday()], font = ('Arial', 24), text_color = 'white')
+    weekday_text.place(x = 700, y = 200)
     list_first_hours_temp = []
     list_first_hours_time = []
+    list_weather_icon2 = []
     for i in range(8):
         first_hours_time = CTkLabel(frame_hourly_weather, text = f'{list_weather_time[i]}:00', font = ('Arial', 18), text_color = 'white')
         first_hours_time.grid(row = 0, column = i, pady = 10, padx = 10)
         list_first_hours_time.append(first_hours_time)
+        path_to_image = os.path.abspath(__file__ + f'/../weather/{list_weather_icon[i]}.png')
+        if not os.path.exists(path_to_image):
+            path_to_image = os.path.abspath(__file__ + f'/../weather/01d.png')
+            print('Не знайдено картинку', list_weather_icon[0])
+        image = PIL.Image.open(path_to_image)
+        image = image.resize([50, 50])
+        image = PIL.ImageTk.PhotoImage(image)
+        label_image = CTkLabel(frame_hourly_weather, image = image, text = '')
+        label_image.grid(row = 1, column = i, pady = 10, padx = 16)
+        list_weather_icon2.append(label_image)
         first_hours_temp = CTkLabel(frame_hourly_weather, text = f'{list_weather_temp[i]}°', font = ('Arial', 26), text_color = 'white')
-        first_hours_temp.grid(row = 1, column = i, pady = 10, padx = 20)
+        first_hours_temp.grid(row = 2, column = i, pady = 10, padx = 16)
         list_first_hours_temp.append(first_hours_temp)
-        # second_hours_time = CTkLabel(frame_hourly_weather, text = f'{list_weather_time[1]}:00', font = ('Arial', 18), text_color = 'white')
-        # second_hours_time.grid(row = 0, column = 1, pady = 10, padx = 10)
-        # second_hours_temp = CTkLabel(frame_hourly_weather, text = f'{list_weather_temp[1]}°', font = ('Arial', 26), text_color = 'white')
-        # second_hours_temp.grid(row = 1, column = 1, pady = 10, padx = 10)
-    # date_time = CTkLabel(frame_current_position, text = , font = ('Arial', 22), text_color = 'white')
+    list_weather_max_min_temp = []
+    for i in range(8):
+        list_weather_max_min_temp.append(list_weather_temp[i])
+    max_temp = str(max(list_weather_max_min_temp)).split('.')[0]
+    min_temp = str(min(list_weather_max_min_temp)).split('.')[0]
+    path_image_arrow_up = os.path.abspath(__file__ + '/../arrow_up.png')
+    image_arrow_up = PIL.Image.open(path_image_arrow_up)
+    image_arrow_up = image_arrow_up.resize([28, 33])
+    image_arrow_up = PIL.ImageTk.PhotoImage(image_arrow_up)
+    path_image_arrow_down = os.path.abspath(__file__ + '/../arrow_down.png')
+    image_arrow_down = PIL.Image.open(path_image_arrow_down)
+    image_arrow_down = image_arrow_down.resize([28, 33])
+    image_arrow_down = PIL.ImageTk.PhotoImage(image_arrow_down)
+    current_max_temp = CTkLabel(frame_weather, text = f'{min_temp}°          {max_temp}°', font = ('Arial', 30), text_color = 'white')
+    current_max_temp.place(x = 370, y = 360)
+    arrow_up = CTkLabel(frame_weather, image = image_arrow_up, text = '', font = ('Arial', 30), text_color = 'white')
+    arrow_up.place(x = 460, y = 360)
+    arrow_down = CTkLabel(frame_weather, image = image_arrow_down, text = '', font = ('Arial', 30), text_color = 'white')
+    arrow_down.place(x = 330, y = 360)
+    # current_max_min_temp = CTkLabel(frame_current_position, text = f'⬇️ {min_temp}°   ⬆️ {max_temp}°', font = ('Arial', 30), text_color = 'white')
+    # current_max_min_temp.grid(row = 4, column = 0, pady = 10)
+    def current_time_func():
+        app.after(1000, current_time_func)
+        current_time_now = str(datetime.now()).split('.')[0].split(' ')[1]
+        current_time_now_text = CTkLabel(frame_weather, text = current_time_now.split('.')[0], font = ('Arial', 30), text_color = 'white')
+        current_time_now_text.place(x = 677, y = 300)
+    current_time_func()
+    frame_current_city = CTkFrame(frame_city_select, 235, 100, fg_color = '#096C82', border_color = '#b5d3d9', border_width = 5, corner_radius = 20)
+    frame_current_city.place(x = 20, y = 30)
+    current_city_position_text = CTkLabel(frame_current_city, text = 'Поточна позиція', font = ('Arial', 16), text_color = 'white')
+    current_city_position_text.place(x = 14, y = 8)
+    current_city_select_text = CTkLabel(frame_current_city, text = user_city, font = ('Arial', 12), text_color = 'white')
+    current_city_select_text.place(x = 15, y = 30)
+    current_temp_select_text = CTkLabel(frame_current_city, text = f'{str(temp).split('.')[0]}°', font = ('Arial', 50), text_color = 'white')
+    current_temp_select_text.place(x = 150, y = 10)
+    current_description_select_text = CTkLabel(frame_current_city, text = weather_desription, font = ('Arial', 13), text_color = 'white')
+    current_description_select_text.place(x = 14, y = 55)
+    current_city_select_max_temp_text = CTkLabel(frame_current_city, text = f'макс: {max_temp}°, мін: {min_temp}°', font = ('Arial', 12), text_color = 'white')
+    current_city_select_max_temp_text.place(x = 120, y = 65)
 if user_name != None:
     load_weather()
+    app.after(1000, current_time_func)
 user_city_search = ''
 def search_city(event):
-    global user_city_search, temp, feels_like, wind_speed, humidity, weather_desription, current_city_text, current_temp_text
+    global user_city_search, temp, feels_like, wind_speed, humidity, weather_desription, current_city_text, current_temp_text, range_count, max_temp, min_temp
     user_city_search = search_entry.get()
     url = f'https://api.openweathermap.org/data/2.5/forecast?q={user_city_search}&appid={api}'
     answer = requests.get(url)
     if answer.status_code == 200:
+        range_count = [0, 1, 2, 3, 4, 5, 6, 7]
         data = answer.json()
         temp = data['list'][0]['main']['temp'] - 273.15
         temp = round(temp, 1)
@@ -186,7 +313,7 @@ def search_city(event):
         weather_desription = data['list'][0]['weather'][0]['description'].title()
         dt_txt = 'dt_txt'
         current_city_text.configure(text = user_city_search)
-        current_temp_text.configure(text = f'{temp}°C')
+        current_temp_text.configure(text = f'{temp}°')
         weather_desription = translator.translate(weather_desription)
         current_description_text.configure(text = weather_desription)
         list_weather_time = []
@@ -197,6 +324,16 @@ def search_city(event):
         for i in range(8):
             list_first_hours_time[i].configure(text = f'{list_weather_time[i]}:00')
             list_first_hours_temp[i].configure(text = f'{list_weather_temp[i]}°')
+        list_weather_max_min_temp = []
+        for i in range(8):
+            list_weather_max_min_temp.append(list_weather_temp[i])
+        current_description_select_text.configure(text = weather_desription)
+        current_city_select_text.configure(text = user_city_search)
+        current_temp_select_text.configure(text = f'{str(temp).split('.')[0]}°')
+        max_temp = str(max(list_weather_max_min_temp)).split('.')[0]
+        min_temp = str(min(list_weather_max_min_temp)).split('.')[0]
+        current_city_select_max_temp_text.configure(text = f'mакс: {max_temp}°, мін: {min_temp}°')
+        current_max_min_temp.configure(text = f'⬇️ {min_temp}°   ⬆️ {max_temp}°')
         # load_weather()
 search_loope.bind('<Button-1>', search_city)
 def open_register(event):
